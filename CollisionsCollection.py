8ff077a2dd3349e3f6a7b8be36641bae7df47c60
epicary22 <epicary22@gmail.com>
2024-05-01 20:50:01 -0500

    start on the collisions collection

---
+++ CollisionsCollection.py
@@ -0,0 +1,8 @@
+class CollisionsCollection:
+	def __init__(self):
+		"""
+		Creates a new CollisionsCollection, an object that can store all of your CollisionsLayers and assign them
+		z-layers and actions upon collision, among other useful properties.
+		"""
+		pass
+		
