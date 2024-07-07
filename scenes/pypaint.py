7f5d5d44a701b0aa541f88f3c9bcdde99a2b465a
epicary22 <epicary22@gmail.com>
2024-07-07 16:04:43 -0500

    add basic command line

--- scenes/pypaint.py
+++ scenes/pypaint.py
@@ -6,9 +6,12 @@ from bitmap_collection import BitmapCollection
 from collision_binder import CollisionBinder
 from renderer import Renderer
 import time
+import random
 
 
 class PyPaint(Scene):
+	BG_COLOR_PAIR = 1
+	
 	def __init__(self):
 		super().__init__()
 		
@@ -18,25 +21,47 @@ class PyPaint(Scene):
 		curses.curs_set(0)
 		curses.resizeterm(24, 80)
 		
-		curses.init_pair(1, 9, 0)
+		curses.init_pair(self.BG_COLOR_PAIR, random.randint(9, 15), 0)
+		curses.init_pair(2, 15, 0)
 		
 		self.bitmaps = BitmapCollection()
 		self.collisions = CollisionBinder(self.bitmaps)
 		self.renderer = Renderer(self.stdscr)
 		
-		# bitmap definitions
+		# menu
 		canvas_size = (16, 56)
-		canvas_border = BitmapLayer(canvas_size[0] + 2, canvas_size[1] + 4, (0, 0))
-		canvas_border.set_all(True)
-		canvas_border.add_rect(False, canvas_size[0], canvas_size[1], (1, 2))
-		canvas_border.lock()
-		self.bitmaps.add(canvas_border, "canvas_border")
-		self.renderer.add(canvas_border, "canvas_border", " ", attrs=curses.A_REVERSE)
+		screen_size = self.stdscr.getmaxyx()
+		
+		menu_border = BitmapLayer(*screen_size, (0, 0))
+		menu_border.set_all(True)
+		# (canvas space)
+		menu_border.add_rect(False, *canvas_size, (1, 2))
+		# (lower menu space)
+		menu_border.add_rect(
+			False,
+			screen_size[0] - canvas_size[0] - 3,
+			canvas_size[1],
+			(canvas_size[0] + 2, 2)
+		)
+		# (rgb flipper space)
+		menu_border.add_rect(
+			False,
+			screen_size[0] - canvas_size[0],
+			screen_size[1] - canvas_size[1] - 6,
+			(canvas_size[0] - 1, canvas_size[1] + 4)
+		)
+		menu_border.lock()
+		self.bitmaps.add(menu_border, "menu_border")
+		self.renderer.add(menu_border, "menu_border", " ", attrs=curses.A_REVERSE, color_pair=1)
+		
+		canvas = BitmapLayer(*canvas_size, (1, 2))
+		self.bitmaps.add(canvas, "canvas")
+		self.renderer.add(canvas, "canvas", " ", z_layer=-1, attrs=curses.A_REVERSE, color_pair=2)
 		
 		cursor = BitmapLayer(1, 1, (canvas_size[0] // 2, canvas_size[1] // 2))
 		cursor.set_all(True)
 		self.bitmaps.add(cursor, "cursor")
-		self.renderer.add(cursor, "cursor", "X", z_layer=-1, attrs=curses.A_NORMAL)
+		self.renderer.add(cursor, "cursor", "X", attrs=curses.A_NORMAL, color_pair=2)
 		
 		# controler
 		self.controller = CursesController(
@@ -45,20 +70,20 @@ class PyPaint(Scene):
 				"h": cursor.pos_vector.left,
 				"j": cursor.pos_vector.down,
 				"k": cursor.pos_vector.up,
-				"l": cursor.pos_vector.right
+				"l": cursor.pos_vector.right,
+				"f": lambda: canvas.set_point(True, *canvas.relative_yx(*cursor.top_left())),
+				"d": lambda: canvas.set_point(False, *canvas.relative_yx(*cursor.top_left())),
+				"c": lambda: canvas.set_all(False),
+				":": self.colon_command
 			}
 		)
 		
 		# collisions
-		self.collisions.bind("cursor", "canvas_border", cursor.pos_vector.cancel_transform)
+		self.collisions.bind("cursor", "menu_border", cursor.pos_vector.cancel_transform)
 		
 	def update(self) -> None:
 		self.stdscr.erase()
-		
 		self.renderer.render()
-		
-		self.stdscr.addstr(str(self.bitmaps.get("cursor").top_left()))
-		
 		self.stdscr.refresh()
 		
 		self.controller.run()
@@ -75,4 +100,34 @@ class PyPaint(Scene):
 				self.stdscr.addstr(2, 0, f"Crashing in {i}...")
 				self.stdscr.refresh()
 				time.sleep(1)
+		
+	def colon_command(self):
+		# out of "game engine" mode
+		curses.echo()
+		curses.nocbreak()
+		self.stdscr.attrset(curses.color_pair(self.BG_COLOR_PAIR) | curses.A_REVERSE)
+		
+		self.stdscr.move(self.stdscr.getmaxyx()[0] - 1, 0)
+		self.stdscr.addstr(":")
+		self.stdscr.refresh()
+		
+		user_input = ""
+		while not user_input:
+			user_input = self.stdscr.getstr()
+		
+		self.stdscr.hline(self.stdscr.getmaxyx()[0] - 1, 0, " ", 100)
+		self.stdscr.addstr(1, 1, user_input, curses.color_pair(self.BG_COLOR_PAIR) | curses.A_REVERSE)
+		self.stdscr.move(self.stdscr.getmaxyx()[0] - 1, 1)
+		self.stdscr.refresh()
+		
+		if user_input == b"star":
+			proof_of_concept = BitmapLayer(3, 6, (2, 4))
+			proof_of_concept.add_rect(True, 3, 2, (0, 2))
+			proof_of_concept.add_rect(True, 1, 6, (1, 0))
+			self.renderer.add(proof_of_concept, "proof_of_concept", " ", attrs=curses.A_REVERSE, color_pair=self.BG_COLOR_PAIR)
+		
+		# back into "game engine" mode
+		curses.noecho()
+		curses.cbreak()
+		self.stdscr.attrset(curses.A_NORMAL)
 		
